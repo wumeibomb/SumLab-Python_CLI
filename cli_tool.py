1c049c6beecd878707e78c87024873ad24f9677a
Amina Masera <aminakavele@gmail.com>
2026-07-19 19:09:48 +0300

    updated cli_tool

--- cli_tool.py
+++ cli_tool.py
@@ -14,11 +14,13 @@ from datetime import datetime
 #Handle exceptions with meaningful error messages, not cryptic stack traces.
 #Avoid overwhelming the user—display only relevant info, and provide examples where helpful.
 
+log_file = "data/user_log.txt"
 class User:
 
     def __init__(self, username, email):
         self.username = username
         self.email = email
+        self._projects = []
         print(username,email)
     
     @property
@@ -30,9 +32,37 @@ class User:
         if not isinstance(value, Project):
             raise TypeError("Must be of the USER class")
         self._projects = value
+    
+    @projects.deleter
+    def projects(self):
+         print("to delete")
+         del self._projects
 
+class Project:
 
+    def __init__(self, title, description, due_date):
+        self.title = title
+        self.description = description
+        self.due = due_date
+        self.tasks = []
+    
+    def assign_project(self, user):
+         user.project = self
+        
+    def add_task(self, task):
+         self.tasks.append(task)
+         print("brih")
 
+class Tasks:
+    def __init__(self, title, status, assigned_to):
+        self.title = title
+        self.status = status
+        self.assigned_to = assigned_to
+        
+    def complete(self):
+        self.completed = True
+        print("task complete!!! feeling fulfilled?")
+   
 projects_list = {}
 email_list = {}
 user_list = {}
@@ -49,26 +79,40 @@ def add_user(args):
 
         def addition_logging(bruh):
             timestamp = datetime.now().strftime("%d-%m-%Y at %H:%M:%S")
-            with open("data/user_log.txt","a") as file:
-              file.write(f"[{timestamp}] - New User Added : {username} & Email : {gmail}\n")
+            with open("data/main.json","a") as file:
+                file.write(f'"username" : "{username}", "email" : "{gmail}"')
+                           #"email" : {gmail}}")
+              #file.write(f"[{timestamp}] - New User Added : {username} & Email : {gmail}\n")
         addition_logging(User)
-        #ITS NOT ADDING MORE TO THE LIST OH MY GOSHHHH
+
 
 def manage_users(args):
-        users = user_list
-        print(users)
+        test = getattr(args,args.USER)
+        print("fsdv", test)
+
+#bruh = User()
 
 def manage_projects(args):
      print(f"{args.username} added {args.title}")
 
+     def project_logging(bruh):
+        if args.title in log_file:
+             with open(log_file, "a") as file:
+                file.write(f"{args.username} added {args.title}\n")
+        else:
+             print("This project is already in the database.")
+        project_logging(Project)
+
+
 def manage_tasks(args):
-     #completion or deletion?
+     #completion
      projects = projects_list.get(args.USER)
      if projects:
           for eachtask in projects.TASK:
                if eachtask.title == args.title:
-                    task.complete()
+                    class_Tasks.complete()
                     return
+     
      
 def add_manage_tasks(args):
     task = (args.pro)
@@ -77,7 +121,7 @@ def add_manage_tasks(args):
 def main():
 
     parser = argparse.ArgumentParser(description= "Python Project Management CLI tool.")
-# options: create user, manage users(display the users) and select them, display projects - assign task to projects and mark complete.
+# options: create user DOME, manage users(display the users) and select them, display projects - assign task to projects and mark complete.
 #if certain user selected, open this set of projects. don't show the others.
     subparsers = parser.add_subparsers()
     add_parser = subparsers.add_parser("New", help="Add a new USER!") #new user, no list of users
@@ -86,7 +130,6 @@ def main():
     add_parser.set_defaults(func=add_user)
 
     manageuser_parser = subparsers.add_parser("Users", help="Lists users")
-    manageuser_parser.add_argument("list", help="list the users")
     manageuser_parser.set_defaults(func=manage_users)
 
     managetasks_parser = subparsers.add_parser("add-task",help="Command to add a task: add-task taskname assignedproject")
@@ -95,10 +138,15 @@ def main():
     managetasks_parser.set_defaults(func=add_manage_tasks)
     #need to complete tasks
 
-    addproject_parser = subparsers.add_parser("add-project", help= "add a project to user")
-    addproject_parser.add_argument("username", help="The projects user")
+    addproject_parser = subparsers.add_parser("add-project", help= "Add a project to user")
     addproject_parser.add_argument("title",help="The project's title")
+    addproject_parser.add_argument("description", help="Description of the project")
+    addproject_parser.add_argument("duedate", help="Due date of the project")
     addproject_parser.set_defaults(func=manage_projects)
+
+    deleteproject_parser = subparsers.add_parser("del-project", help= "to delete project")
+    deleteproject_parser.add_argument("PROJECT", help="Project to delete")
+    deleteproject_parser.set_defaults(func=Project.)
     #list projects under a user eg flopster has added this, then show othger projects if any.
     #add tasks to projects
 
